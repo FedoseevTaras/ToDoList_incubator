1b1d43857064d7dc7365a35f8406091c4845a113
Taras <fedoseevt@gmail.com>
2022-02-10 14:14:04 +0200

    Updates

---
+++ precache-manifest.955b2fca98dc622f10b79d7572e76e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d14b2e265cba2fc89c5aafd623d3c4",
+    "url": "/ToDoList_incubator/index.html"
+  },
+  {
+    "revision": "1e26c1727d2120022e0d",
+    "url": "/ToDoList_incubator/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "3649be2dc8b8e3193387",
+    "url": "/ToDoList_incubator/static/js/2.d99582fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList_incubator/static/js/2.d99582fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e26c1727d2120022e0d",
+    "url": "/ToDoList_incubator/static/js/main.4aaf7b80.chunk.js"
+  },
+  {
+    "revision": "671e4ca8bc6b1c197bf3",
+    "url": "/ToDoList_incubator/static/js/runtime-main.f54b86c3.js"
+  }
+]);
